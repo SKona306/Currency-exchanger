4dd292d6e414bdc2db17b0aed370eaa0401e84c7
Shaun Kent <shaunkent81@gmail.com>
2021-08-14 13:57:21 -0700

    add static method for fetching api data

---
+++ src/staticexchange.js
@@ -0,0 +1,14 @@
+export class searchExchangeRate {
+  static getRate(targetCurrency, amount) {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${targetCurrency}/${amount}`)
+      .then(function(response) {
+        if(!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .catch(function(error) {
+        return error;
+      });
+  }
+}
